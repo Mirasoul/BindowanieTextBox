19d4769935dcacad526a34c244952f0640b0cefc
MarekGmurczyk <MarekGmurczyk@MGMURCZYK-DELL>
2022-12-16 09:58:16 +0100

    Dodaj pliki projektów.

---
+++ BindowanieTextBox/Model/CzynnosciCzlowiek.cs
@@ -0,0 +1,24 @@
+﻿namespace BindowanieTextBox
+{
+    interface ICzynnosciCzlowiek
+    {
+        string Mowi();
+        string Pisze();
+        string Chodzi();
+    }
+    public class CzynnosciCzlowiek: ICzynnosciCzlowiek
+    {
+        public string Mowi()
+        {
+            return "Mowi";
+        }
+        public string Pisze()
+        {
+            return "Pisze";
+        }
+        public string Chodzi()
+        {
+            return "Chodzi";
+        }
+    }
+}
---
+++ BindowanieTextBox/ViewModel/Czlowiek.cs
@@ -0,0 +1,56 @@
+﻿namespace BindowanieTextBox
+{
+ 
+    public struct CechyCzlowiek
+    {
+        // public byte Age;
+        // public byte Height;
+        public string Name;
+        public string Surname;
+    }
+
+    public class Czlowiek
+    {    
+        // private string _imie;
+        private CechyCzlowiek _imie;
+        private CechyCzlowiek _nazwisko;
+        private string _dzialanie;
+
+        public string ImieNaszegoCzlowieka
+        {
+            get
+            {
+                return _imie.Name;
+            }
+
+        }
+        public string NazwiskoNaszegoCzlowieka
+        {
+            get
+            {
+                return _nazwisko.Surname;
+            }
+
+        }
+        public string DzialanieNaszegoCzlowieka
+        {
+            get
+            {
+                return _dzialanie;
+            }
+            set
+            {
+                _dzialanie = value;
+            }
+        }
+
+        public Czlowiek()
+        {
+            CzynnosciCzlowiek _czynnosci = new CzynnosciCzlowiek();
+            _dzialanie = _czynnosci.Chodzi();
+            _imie.Name = "Arkadiusz";
+            _nazwisko.Surname = "Brzeczyszczykiewicz";
+        }
+
+    }
+}
